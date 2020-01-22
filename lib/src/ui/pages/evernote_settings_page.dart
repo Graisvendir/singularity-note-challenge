8a377775c91e87358e59ffdef0cce0edbd2a5517
Elena Popova <44632346+fogelvogel@users.noreply.github.com>
2020-01-22 20:58:11 +0700

    все поля сохраняются

--- lib/src/ui/pages/evernote_settings_page.dart
+++ lib/src/ui/pages/evernote_settings_page.dart
@@ -6,20 +6,71 @@ import 'package:note_project/src/ui/pages/settings_props/save_button.dart';
 import 'package:provider/provider.dart';
 import 'package:note_project/src/blocks/settings_bloc.dart';
 
-class EvernoteSettingsPage extends StatelessWidget {
+class EvernoteSettingsPage extends StatefulWidget {
+  @override
+  _EvernoteSettingsPageState createState() => _EvernoteSettingsPageState();
+}
+
+class _EvernoteSettingsPageState extends State<EvernoteSettingsPage> {
+  bool checkValue = false;
+  TextEditingController evernoteController;
+  SettingsBloc bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    final repository = Provider.of<Repository>(context, listen: false);
+    bloc = SettingsBloc(repository);
+    evernoteController = TextEditingController();
+
+    bloc.fetchSettings().then((value) {
+      evernoteController.text = bloc.settings[Settings.evernote].value;
+    });
+  }
+
+   @override
+    void dispose() {
+    evernoteController.dispose();
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final repository = Provider.of<Repository>(context);
-
-    var provider = Provider<SettingsBloc>(
-      create: (context) => SettingsBloc(repository)..fetchSettings(),
-      dispose: (context, bloc) => bloc.dispose(),
+    var provider = Provider.value(
+      value: bloc,
       child: SafeArea(
         child: Scaffold(
           body: ListView(
             children: <Widget>[
-              SettingsElements(),
-              SaveButton(),
+              Column(
+                children: <Widget>[
+                  Text('Evernote Setting'),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: evernoteController,
+                  ), 
+                  StreamBuilder(
+                    stream: bloc.settings[Settings.alwaysSyncEvernote],
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) return Container();
+                      return Checkbox(
+                        value: snapshot.data,
+                        onChanged: (bool value) {
+                            bloc.put(Settings.alwaysSyncEvernote, value);
+                        },
+                      );
+                    }
+                  )
+                ],
+              ),
+              SaveButton(
+                saveCallback: () {
+                  bloc.put(Settings.evernote, evernoteController.value.text);
+                },
+              ),
               DeleteSync()
             ],
           ),
@@ -27,44 +78,5 @@ class EvernoteSettingsPage extends StatelessWidget {
       )
     );
     return provider;
-  }
-}
-
-class SettingsElements extends StatefulWidget {
-
-  @override
-  _EvernoteSettingsState createState() => _EvernoteSettingsState();
-}
-
-class _EvernoteSettingsState extends State<SettingsElements> {
-
-  bool checkValue = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final settingsBloc = Provider.of<SettingsBloc>(context);
-    return Column(
-      children: <Widget>[
-        Text('Evernote Setting'),
-        TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-          ),
-        ), 
-        StreamBuilder(
-          stream: settingsBloc.settings[Settings.alwaysSyncEvernote],
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return Container();
-            return Checkbox(
-              value: snapshot.data,
-              onChanged: (bool value) {
-                  settingsBloc.put(Settings.alwaysSyncEvernote, value);
-              },
-            );
-          }
-        )
-      ],
-    );
   }
 }
--- lib/src/ui/pages/singularity_settings_page.dart
+++ lib/src/ui/pages/singularity_settings_page.dart
@@ -6,20 +6,85 @@ import 'package:note_project/src/ui/pages/settings_props/save_button.dart';
 import 'package:provider/provider.dart';
 import 'package:note_project/src/blocks/settings_bloc.dart';
 
-class SingularitySettingsPage extends StatelessWidget {
+class SingularitySettingsPage extends StatefulWidget {
+  @override
+  _SingularitySettingsPageState createState() => _SingularitySettingsPageState();
+}
+
+class _SingularitySettingsPageState extends State<SingularitySettingsPage> {
+
+  bool checkValue = false;
+  TextEditingController singularityLoginController;
+  TextEditingController singularityPassController;
+  SettingsBloc bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    final repository = Provider.of<Repository>(context, listen: false);
+    bloc = SettingsBloc(repository);
+    singularityLoginController = TextEditingController();
+    singularityPassController = TextEditingController();
+
+    bloc.fetchSettings().then((value) {
+      singularityLoginController.text = bloc.settings[Settings.singLogin].value;
+      singularityPassController.text = bloc.settings[Settings.singPass].value;
+    });
+  }
+
+  @override
+  void dispose() {
+    singularityLoginController.dispose();
+    singularityPassController.dispose();
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final repository = Provider.of<Repository>(context);
-
-    var provider = Provider<SettingsBloc>(
-      create: (context) => SettingsBloc(repository)..fetchSettings(),
-      dispose: (context, bloc) => bloc.dispose(),
+    var provider = Provider.value(
+      value: bloc,
       child: SafeArea(
         child: Scaffold(
           body: ListView(
             children: <Widget>[
-              SettingsElements(),
-              SaveButton(),
+              Column(
+                children: <Widget>[
+                  Text('SingularityApp Setting'),
+                  //логин
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: singularityLoginController,
+                  ),
+                  // пароль пока не делаем, не понятно что будет
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: singularityPassController,
+                  ),  
+                  StreamBuilder(
+                    stream: bloc.settings[Settings.alwaysSyncSIngularity],
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) return Container();
+                      return Checkbox(
+                        value: snapshot.data,
+                        onChanged: (bool value) {
+                            bloc.put(Settings.alwaysSyncSIngularity, value);
+                        },
+                      );
+                    }
+                  )
+                ],
+              ),
+              SaveButton(
+                saveCallback: () {
+                  bloc.put(Settings.singLogin, singularityLoginController.value.text);
+                  bloc.put(Settings.singPass, singularityPassController.value.text);
+                },
+              ),
               DeleteSync()
             ],
           ),
@@ -27,76 +92,5 @@ class SingularitySettingsPage extends StatelessWidget {
       )
     );
     return provider;
-  }
-}
-
-class SettingsElements extends StatefulWidget {
-
-  @override
-  _SingularitySettingsState createState() => _SingularitySettingsState();
-}
-
-class _SingularitySettingsState extends State<SettingsElements> {
-
-  bool checkValue = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final settingsBloc = Provider.of<SettingsBloc>(context);
-    return Column(
-      children: <Widget>[
-        Text('Singularity App Setting'),
-        TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-          ),
-        ),
-        TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-          )
-        ), 
-        StreamBuilder(
-          stream: settingsBloc.settings[Settings.alwaysSyncSIngularity],
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return Container();
-            return Checkbox(
-              value: snapshot.data,
-              onChanged: (bool value) {
-                  settingsBloc.put(Settings.alwaysSyncSIngularity, value);
-              },
-            );
-          }
-        )
-      ],
-    );
-    // return Column(
-    //   children: <Widget>[
-    //     Text('Singularity App Setting'),
-    //     TextField(
-    //       obscureText: true,
-    //       decoration: InputDecoration(
-    //         border: OutlineInputBorder(),
-    //       ),
-         
-    //     ),
-    //     TextField(
-    //       obscureText: true,
-    //       decoration: InputDecoration(
-    //         border: OutlineInputBorder(),
-    //       )
-    //     ), 
-    //     Checkbox(
-    //       value: checkValue,
-    //         onChanged: (bool value) {
-    //             setState(() {
-    //                 checkValue = value;
-    //             });
-    //         },
-    //     )
-    //   ],
-    // );
   }
 }
