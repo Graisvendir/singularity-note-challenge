5b4e9a39061ef56905b84d0d8ac2fda87d637b59
Elena Popova <44632346+fogelvogel@users.noreply.github.com>
2020-01-31 21:20:30 +0700

    + приличные тексты

--- lib/src/resources/localisation.dart
+++ lib/src/resources/localisation.dart
@@ -2,23 +2,26 @@ import 'package:flutter/widgets.dart';
 
 const LIGHT_THEME = ['Light theme', 'Светлая тема'];
 const DARK_THEME = ['Dark theme', 'Темная тема'];
-const SING_APP_TRY_NOW = ['Singularity App — try now', 'Singularity App — попробуйте сейчас'];
-const AVAILABLE = ['Available for all platforms', 'Доступно для всех платформ'];
-const SWIPE_UP = ['Swipe up to send', 'Свайп вверх чтобы отправить'];
-const SWIPE_DOWN = ['Swipe down to thrash', 'Свайп вниз чтобы удалить'];
-const SWIPE_LEFT = ['Swipe left to see all notes', 'Свайп влево — список заметок'];
-const SWIPE_RIGHT = ['Swipe right to make settings', 'Свайп вправо — настройки'];
+const SING_APP_TRY_NOW = ['Singularity App — try now', 'Попробуй SingularityApp'];
+const AVAILABLE = ['Available for all platforms', 'Доступно на всех платформах'];
+const SWIPE_UP = ['Swipe up to send', 'Свайп вверх для отправки'];
+const SWIPE_DOWN = ['Swipe down to trash', 'Свайп вниз для удаления заметки'];
+const SWIPE_LEFT = ['Swipe left to see all notes', 'Свайп влево ко всем заметкам'];
+const SWIPE_RIGHT = ['Swipe right to make settings', 'Свайп вправо к Настройкам'];
 const LOGIN = ['Login', 'Логин'];
 const PASS = ['Password', 'Пароль'];
-const ALWAYS_SYNC = ['Always sync', 'Синхронизация включена'];
+const ALWAYS_SYNC = ['Always sync', 'Всегда синхронизировать'];
 const SAVE = ['Save', 'Сохранить'];
-const DELETE_SYNC = ['Delete this sync', 'Удалить синхронизацию'];
-const EMAIL_ADRESS = ['Email Adress', 'Адрес почты'];
-const EMAIL_SETTINGS = ['Email Settings', 'Настройки почты'];
-const SING_SETTINGS = ['Singularity App Settings', 'Настройки Singularity App'];
+const DELETE_SYNC = ['Delete this sync', 'Удалить эту синхронизацию'];
+const EMAIL_ADRESS = ['Email Adress', 'Email'];
+const EMAIL_SETTINGS = ['Email Settings', 'Настройки Email'];
+const SING_SETTINGS = ['SingularityApp Settings', 'Настройки SingularityApp'];
 const EVERNOTE_SETTINGS = ['Evernote Settings', 'Настройки Evernote'];
+const REQUIRED_FIELD = ['Required field', 'Обязательное поле'];
+const EMAIL_NOT_CORRECT = ['Email is not correct', 'Некорректный Email'];
+const NO_NOTES = ['There are no notes yet', 'У вас пока нет заметок'];
 
-String localize(List<String> textConstant, context) {
+String localize(List<String> textConstant, BuildContext context) {
   Locale myLocale = Localizations.localeOf(context);
   print(myLocale.languageCode);
   print(myLocale.countryCode);
